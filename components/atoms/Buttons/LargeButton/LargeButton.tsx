85f000f5c88a6359ed1b2dcb52b7dc1a2216754c
JoaoVerissimo <joao17andre@hotmail.com>
2022-07-30 20:34:32 +0100

    Add random code to test pre-commit

--- components/atoms/Buttons/LargeButton/LargeButton.tsx
+++ components/atoms/Buttons/LargeButton/LargeButton.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 type Props = {}
 
+debugger
+
 export const LargeButton: React.FC<Props> = (props) => {
-  return <div className="" data-testid="test" />
+  return (
+    <div className="" data-testid="test">
+      <h1>Large Button</h1>;
+    </div>
+  )
 }
